88965ee9e14ef1845e89ad12d2b35d889644e31f
Maciej Nikiel <m.nikiel@gmail.com>
2020-04-20 11:43:44 +0200

    Initial commit

---
+++ src/test/java/edu/agh/zp/ZpApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.agh.zp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
