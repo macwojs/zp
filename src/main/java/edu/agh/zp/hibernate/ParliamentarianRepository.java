0af46d52b5fce1a3e819a93b9696c594b8000e93
GrzegorzDomaradzki <grzedom@onet.pl>
2020-04-27 22:15:56 +0200

    Repositories added

---
+++ src/main/java/edu/agh/zp/hibernate/ParliamentarianRepository.java
@@ -0,0 +1,6 @@
+package edu.agh.zp.hibernate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParliamentarianRepository extends JpaRepository<ParliamentarianEntity, Long> {
+}
