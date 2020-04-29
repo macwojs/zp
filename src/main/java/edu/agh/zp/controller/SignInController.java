0423856900ca18628de95fe1fbff306877b93a81
pburkiewicz <burkiewicz.patrycja@gmail.com>
2020-04-29 18:11:27 +0200

    Possibility to enter the SignIn tab, refactor

--- src/main/java/edu/agh/zp/controller/SignInController.java
+++ src/main/java/edu/agh/zp/controller/SignInController.java
@@ -1,60 +1,44 @@
 package edu.agh.zp.controller;
 
-import edu.agh.zp.objects.CitizenEntity;
+import edu.agh.zp.hibernate.CitizenEntity;
 import edu.agh.zp.hibernate.CitizenRepository;
 import edu.agh.zp.objects.User;
 import edu.agh.zp.services.CitizenService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
-import edu.agh.zp.SecurityConfig;
-import org.springframework.beans.factory.*;
-
-import javax.validation.Valid;
+import edu.agh.zp.services.CitizenService;
+import javax.activation.DataHandler;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
-@RequestMapping (value={"/singup"})
-public class SingUpController {
-	@Autowired
-	private ApplicationContext context;
+@RequestMapping (value={"/signin"})
+public class SignInController {
 	private CitizenService cS;
+	private ApplicationContext context;
 
 	@GetMapping (value = {""})
 	public ModelAndView index() {
-		String viewName = "singup";
+		String viewName = "signin" ;
 		Map<String, Object> model = new HashMap<String, Object>();
 		model.put("user", new User());
-
 		return new ModelAndView(viewName, model);
 	}
+
 	@PostMapping("")
-	public ModelAndView submitRegister( @Valid @ModelAttribute("user") User user, BindingResult res){
+	public ModelAndView submitLogin(@ModelAttribute("user") User user, BindingResult res){
 		if( res.hasErrors()){
-			return new ModelAndView("singup");
+
 		}else{
-			CitizenEntity citizen = new CitizenEntity();
-			citizen.setHash(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
-			citizen.setPesel(user.getPesel());
-			citizen.setIdNumber(user.getIdnumber());
-			CitizenRepository repo = context.getBean(CitizenRepository.class);
-			cS =  new CitizenService(context.getBean(CitizenRepository.class));
-			repo.save(citizen);
-
-			//cS.create(citizen);
-
+			//cS =  new CitizenService(context.getBean(CitizenRepository.class));
 		}
 		RedirectView redirect = new RedirectView();
 		redirect.setUrl("");
---
+++ src/main/java/edu/agh/zp/controller/SignUpController.java
@@ -0,0 +1,63 @@
+package edu.agh.zp.controller;
+
+import edu.agh.zp.objects.CitizenEntity;
+import edu.agh.zp.hibernate.CitizenRepository;
+import edu.agh.zp.objects.User;
+import edu.agh.zp.services.CitizenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+import edu.agh.zp.SecurityConfig;
+import org.springframework.beans.factory.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping (value={"/signup"})
+public class SignUpController {
+	@Autowired
+	private ApplicationContext context;
+	private CitizenService cS;
+
+	@GetMapping (value = {""})
+	public ModelAndView index() {
+		String viewName = "signup";
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("user", new User());
+
+		return new ModelAndView(viewName, model);
+	}
+	@PostMapping("")
+	public ModelAndView submitRegister( @Valid @ModelAttribute("user") User user, BindingResult res){
+		if( res.hasErrors()){
+			return new ModelAndView("singup");
+		}else{
+			CitizenEntity citizen = new CitizenEntity();
+			citizen.setHash(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
+			citizen.setPesel(user.getPesel());
+			citizen.setIdNumber(user.getIdnumber());
+			CitizenRepository repo = context.getBean(CitizenRepository.class);
+			cS =  new CitizenService(context.getBean(CitizenRepository.class));
+			repo.save(citizen);
+
+			//cS.create(citizen);
+
+		}
+		RedirectView redirect = new RedirectView();
+		redirect.setUrl("");
+		return new ModelAndView(redirect);
+	}
+}
