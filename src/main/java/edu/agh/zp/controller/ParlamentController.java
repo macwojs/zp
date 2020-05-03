f184d260d8b79bb4220b016f95c0d6adc7b6579c
Maciej <m.nikiel@gmail.com>
2020-05-03 16:26:46 +0200

    Upload works, files upload to my disc

--- src/main/java/edu/agh/zp/controller/ParlamentController.java
+++ src/main/java/edu/agh/zp/controller/ParlamentController.java
@@ -12,15 +12,18 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.swing.text.Document;
 import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +31,9 @@ import java.util.List;
 @Controller
 @RequestMapping ( value = { "/parlament" } )
 public class ParlamentController {
+
+	private static String UPLOADED_FOLDER = "/home/maciek/Temp/";
+	//private static String UPLOADED_FOLDER = "/static/uploaded/";
 
 	private DocumentTypeRepository documentTypeRepository;
 	private DocumentStatusRepository documentStatusRepository;
@@ -72,11 +78,33 @@ public class ParlamentController {
 	}
 
 	@PostMapping ( value = { "/documentForm" })
-	public ModelAndView submitRegister( @Valid @ModelAttribute("document") DocumentEntity document, BindingResult res){
+	public ModelAndView submitRegister(
+			@RequestParam("file") MultipartFile file,
+			RedirectAttributes redirectAttributes,
+			@Valid @ModelAttribute("document") DocumentEntity document,
+			BindingResult res){
 		if( res.hasErrors()){
 			return new ModelAndView("documentForm");
-		}else{
-			documentRepository.save( document );
+		}
+
+		if(file.isEmpty()){
+			return new ModelAndView("documentForm");
+		}
+
+		documentRepository.save( document );
+
+		try {
+
+			// Get the file and save it somewhere
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+			Files.write(path, bytes);
+
+			redirectAttributes.addFlashAttribute("message",
+					"You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+		} catch ( IOException e) {
+			e.printStackTrace();
 		}
 
 		RedirectView redirect = new RedirectView();
