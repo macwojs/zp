91b27c67dc9295b5325af4a151fb6a57e9b62d3c
GrzegorzDomaradzki <grzedom@onet.pl>
2020-05-06 16:10:42 +0200

    Nothing is working properly but I need a merge. I will crucify the next person which will tell me to make JS script in springboot.

--- src/main/java/edu/agh/zp/controller/GlosowaniaController.java
+++ src/main/java/edu/agh/zp/controller/GlosowaniaController.java
@@ -1,5 +1,11 @@
 package edu.agh.zp.controller;
 
+import edu.agh.zp.objects.VotingEntity;
+import edu.agh.zp.repositories.OptionRepository;
+import edu.agh.zp.repositories.OptionSetRepository;
+import edu.agh.zp.repositories.SetRepository;
+import edu.agh.zp.repositories.VotingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +15,30 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping (value={"/glosowania"})
 public class GlosowaniaController {
 
+	@Autowired
+	VotingRepository votingSession;
+
+	@Autowired
+	OptionSetRepository optionSetSession;
+
+	@Autowired
+	OptionRepository optionSession;
+
+	@Autowired
+	SetRepository setSession;
+
 	@GetMapping (value = {""})
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView( );
 		modelAndView.setViewName( "glosowania" );
+		return modelAndView;
+	}
+
+	@GetMapping (value= {"/prezydenckie"})
+	public ModelAndView prezydentForm() {
+		ModelAndView modelAndView = new ModelAndView( );
+		modelAndView.setViewName( "presidentVotingAdd" );
+		modelAndView.addObject(new VotingEntity());
 		return modelAndView;
 	}
 }
---
+++ src/main/resources/static/js/Lines.js
@@ -0,0 +1,29 @@
+CandidateNum=1;
+
+
+function addElem(){
+    const Div = document.createElement('div');
+    Div.className = 'form-group';
+    Div.id = 'Candidate' + ++CandidateNum;
+    const input = document.createElement('input');
+    input.className='form-control'
+    input.name = 'optionName' + CandidateNum;
+    Div.appendChild(input);
+    const a = document.createElement("a");
+    a.href='#';
+    const para = CandidateNum
+    a.onclick=function() {deleteElem(para)};
+    const img = document.createElement("img");
+    img.src="../static/img/close.png"
+    img.height=14;
+    img.width=14;
+    a.appendChild(img);
+    Div.appendChild(a);
+    document.getElementById('toAdd').appendChild(Div);
+}
+
+function deleteElem(number){
+    const line = document.getElementById("Candidate" + number);
+    document.getElementById('toAdd').removeChild(line);
+    line.remove();
+}
