90ca11b41c5e2a0943a16ce447c4e0bc6d0bd4ec
GrzegorzDomaradzki <grzedom@onet.pl>
2020-05-08 18:50:09 +0200

    presidentVotingAdd works, error info isn't the prettiest, but work.

--- src/main/java/edu/agh/zp/controller/GlosowaniaController.java
+++ src/main/java/edu/agh/zp/controller/GlosowaniaController.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Map;
 
 @Controller
@@ -47,23 +48,27 @@ public class GlosowaniaController {
 	@GetMapping (value= {"/prezydenckie/plan"})
 	public ModelAndView prezydentForm() {
 		ModelAndView modelAndView = new ModelAndView( );
+		modelAndView.addObject("ErrorList", null);
 		modelAndView.setViewName( "presidentVotingAdd" );
-		modelAndView.addObject(new VotingEntity());
 		return modelAndView;
 	}
 
 	@PostMapping(value= {"/prezydenckie/planAdd"})
-	public ModelAndView prezydentSubmit(@RequestParam Map<String,String>reqParameters) throws ParseException {
+	public ModelAndView prezydentSubmit(@RequestParam Map<String,String>reqParameters) throws ParseException
+	{
+		ModelAndView modelAndView = new ModelAndView( );
+		modelAndView.setViewName( "glosowania" );
 		String data = reqParameters.remove("date");
 		SetEntity set = new SetEntity("Wybory Prezydenckie "+ data);
-		setSession.save(set);
-		for (Map.Entry<String,String> entry: reqParameters.entrySet()) {
-			if(entry.getKey().equals("_csrf")) continue;
-			OptionEntity option = new OptionEntity(entry.getValue());
-			optionSession.save(option);
-			optionSetSession.save(new OptionSetEntity(option,set));
+		LocalDate time = timeVerify(data,7); //TODO time verify
+		if (time == null || reqParameters.size()<3 || reqParameters.containsValue(""))
+		{
+			ArrayList<String> errors =  errorsMsg(time, 7,reqParameters);
+			modelAndView.setViewName( "presidentVotingAdd" );
+			modelAndView.addObject("ErrorList",errors);
+			return modelAndView;
 		}
-		LocalDate time = LocalDate.parse(data); //TODO time verify
+		setSession.save(set);
 		votingSession.save(new VotingEntity(
 				java.sql.Date.valueOf(time),
 				java.sql.Time.valueOf(LocalTime.parse("06:00:00")),
@@ -72,10 +77,42 @@ public class GlosowaniaController {
 				null,
 				VotingEntity.TypeOfVoting.PREZYDENT,
 				"Wybory Prezydenckie "+data));
-		ModelAndView modelAndView = new ModelAndView( );
-		modelAndView.setViewName( "glosowania" );
-		modelAndView.addObject(new VotingEntity());
+		for (Map.Entry<String,String> entry: reqParameters.entrySet()) {
+			if(entry.getKey().equals("_csrf")) continue;
+			OptionEntity option = new OptionEntity(entry.getValue());
+			optionSession.save(option);
+			optionSetSession.save(new OptionSetEntity(option,set));
+		}
 		return modelAndView;
 	}
+
+	public LocalDate timeVerify( String time ,int delay)
+	{
+		if (time.isEmpty()) return null;
+		LocalDate now = java.time.LocalDate.now();
+		now = now.plusDays(delay);
+		LocalDate res = LocalDate.parse(time);
+		if (now.isAfter(res)) return  null;
+		return res;
+	}
+
+	public ArrayList<String> errorsMsg(LocalDate date,int delay,Map<String,String>param )
+	{
+		ArrayList<String> res = new ArrayList<String>();
+		if (date==null)
+		{
+			res.add("wydarzenie musi być zaplanowane z " + delay + " dniowym wyprzedzeniem\n");
+		}
+		if (param.size()<3)
+		{
+			res.add("musisz podać przynajmnej 2 kandydatów\n");
+		}
+		if (param.containsValue(""))
+		{
+			res.add("dane kandydata nie mogą być puste\n");
+		}
+		return res;
+	}
+
 }
 
