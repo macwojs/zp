00c5bc818d8d0da99d60157fce51dbe351011e6a
Maciej <m.nikiel@gmail.com>
2020-05-13 12:19:00 +0200

    First scratch form valid - current user already vote?

--- src/main/java/edu/agh/zp/controller/SejmController.java
+++ src/main/java/edu/agh/zp/controller/SejmController.java
@@ -1,22 +1,17 @@
 package edu.agh.zp.controller;
 
-import edu.agh.zp.objects.DocumentEntity;
-import edu.agh.zp.objects.SetEntity;
-import edu.agh.zp.objects.VotingEntity;
-import edu.agh.zp.repositories.DocumentRepository;
-import edu.agh.zp.repositories.SetRepository;
-import edu.agh.zp.repositories.VotingRepository;
+import edu.agh.zp.objects.*;
+import edu.agh.zp.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.security.Principal;
 import java.sql.Time;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -35,7 +30,13 @@ public class SejmController {
 	private DocumentRepository documentRepository;
 
 	@Autowired
+	private final CitizenRepository citizenRepository;
+
+	@Autowired
 	private VotingRepository votingRepository;
+
+	@Autowired
+	private final VoteRepository voteRepository;
 
 	@GetMapping ( value = { "" } )
 	public ModelAndView index() {
@@ -46,11 +47,11 @@ public class SejmController {
 
 	@GetMapping ( value = { "/voteAdd" } )
 	public ModelAndView sejmVoteAdd( ModelAndView model ) {
-		List< DocumentEntity > documents = documentRepository.findByDocForSejm();
-		Optional< SetEntity > set = setRepository.findById((long)1);
-		if(set.isPresent()) {
-			model.addObject("documents", documents);
-			model.addObject("voting", new VotingEntity());
+		List< DocumentEntity > documents = documentRepository.findByDocForSejm( );
+		Optional< SetEntity > set = setRepository.findById( (long) 1 );
+		if ( set.isPresent( ) ) {
+			model.addObject( "documents", documents );
+			model.addObject( "voting", new VotingEntity( ) );
 		}
 		model.setViewName( "sejmVotingAdd" );
 		return model;
@@ -59,37 +60,44 @@ public class SejmController {
 	@PostMapping ( value = { "/voteAdd" } )
 	public ModelAndView documentFormSubmit( @Valid @ModelAttribute ( "voting" ) VotingEntity voting, BindingResult res ) throws ParseException {
 		if ( res.hasErrors( ) ) {
-			for( Object i : res.getAllErrors()){
-				System.out.print("\n"+i.toString()+"\n");
+			for ( Object i : res.getAllErrors( ) ) {
+				System.out.print( "\n" + i.toString( ) + "\n" );
 			}
 
 			//Musi być ponownie dodane, bo inaczej nie wypełnia listy
-			ModelAndView model = new ModelAndView(  );
-			List< DocumentEntity > documents = documentRepository.findByDocForSejm();
-			model.addObject("documents", documents);
+			ModelAndView model = new ModelAndView( );
+			List< DocumentEntity > documents = documentRepository.findByDocForSejm( );
+			model.addObject( "documents", documents );
 			model.setViewName( "sejmVotingAdd" );
 			return model;
 		}
-		Optional< SetEntity > set = setRepository.findById( (long)1 );
-		if(set.isPresent()) {
-			voting.setSetID_column(set.get());
-			DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+		Optional< SetEntity > set = setRepository.findById( (long) 1 );
+		if ( set.isPresent( ) ) {
+			voting.setSetID_column( set.get( ) );
+			DateFormat formatter = new SimpleDateFormat( "HH:mm:ss" );
 
-			Time timeValueOpen = new Time(formatter.parse(voting.getOpen()).getTime());
-			Time timeValueClose = new Time(formatter.parse(voting.getClose()).getTime());
-			voting.setCloseVoting(timeValueClose);
-			voting.setOpenVoting(timeValueOpen);
+			Time timeValueOpen = new Time( formatter.parse( voting.getOpen( ) ).getTime( ) );
+			Time timeValueClose = new Time( formatter.parse( voting.getClose( ) ).getTime( ) );
+			voting.setCloseVoting( timeValueClose );
+			voting.setOpenVoting( timeValueOpen );
 
-			voting.setVotingType(VotingEntity.TypeOfVoting.SEJM);
-			votingRepository.save(voting);
+			voting.setVotingType( VotingEntity.TypeOfVoting.SEJM );
+			votingRepository.save( voting );
 		}
 		RedirectView redirect = new RedirectView( );
 		redirect.setUrl( "/parlament/sejm" );
 		return new ModelAndView( redirect );
 	}
 
-	@GetMapping ( value = { "/vote" } )
-	public ModelAndView sejmVote( ModelAndView model ) {
+	@GetMapping ( value = { "/vote/{num}" } )
+	public ModelAndView sejmVote( ModelAndView model, Principal principal, @PathVariable Long num, final HttpServletRequest request ) {
+		Optional< CitizenEntity > optCurUser = citizenRepository.findByEmail( principal.getName( ));
+		Optional< VoteEntity > vote = voteRepository.findByCitizenIdVotingId( num, optCurUser.get( ).getCitizenID( ) );
+		if ( vote.isPresent() ){
+			final String referer = request.getHeader( "referer" );
+			model.setViewName( referer );
+		}
+
 		model.setViewName( "parliamentVoting" );
 		return model;
 	}
