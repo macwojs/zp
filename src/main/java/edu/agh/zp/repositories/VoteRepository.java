a9fe44286ae904d3c39c7346719eae0b9add2fd3
Maciej <m.nikiel@gmail.com>
2020-05-13 12:18:30 +0200

    Add query to find vote in votes from currentu user in current voting

--- src/main/java/edu/agh/zp/repositories/VoteRepository.java
+++ src/main/java/edu/agh/zp/repositories/VoteRepository.java
@@ -1,7 +1,15 @@
 package edu.agh.zp.repositories;
 
+import edu.agh.zp.objects.CitizenEntity;
 import edu.agh.zp.objects.VoteEntity;
+import edu.agh.zp.objects.VotingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface VoteRepository extends JpaRepository<VoteEntity, Long> {
+	@Query (value="SELECT u FROM vote u WHERE u.votingid=?1 and u.citizenid=?2")
+	Optional<VoteEntity> findByCitizenIdVotingId( long voting, long citizen );
 }
