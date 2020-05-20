551e05b00031d930f823bf17f4bc4ec77ee740a9
pburkiewicz <burkiewicz.patrycja@gmail.com>
2020-05-20 14:42:05 +0200

    Add Parliamentarian to Initializer. Add more sample data.

--- src/main/java/edu/agh/zp/configuration/DBInitializer.java
+++ src/main/java/edu/agh/zp/configuration/DBInitializer.java
@@ -1,11 +1,13 @@
 package edu.agh.zp.configuration;
 
 import edu.agh.zp.objects.CitizenEntity;
+import edu.agh.zp.objects.ParliamentarianEntity;
 import edu.agh.zp.objects.PoliticianEntity;
 import edu.agh.zp.objects.Role;
 import edu.agh.zp.repositories.PoliticianRepository;
 import edu.agh.zp.repositories.RoleRepository;
 import edu.agh.zp.services.CitizenService;
+import edu.agh.zp.services.ParliamentarianService;
 import edu.agh.zp.services.PoliticianService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -25,13 +27,28 @@ public class DBInitializer implements CommandLineRunner {
     @Autowired
     private RoleRepository rR;
     @Autowired
-    private PoliticianService pS;
+    private PoliticianService poS;
+    @Autowired
+    private ParliamentarianService paS;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         createSampleAdmin("admin@zp.pl", "adminadmin","Admin", "Admin", "00010100190", "ARY546695");
         createSampleUser("user@zp.pl", "useruser","User", "User", "00010133000", "AOS266716");
-        createSampleMarszalekSejmu("marszaleksejmu@zp.pl", "marszalekmarszalek","Marszalek", "Sejmu", "00010102260", "AEX304345");
+        createSampleMarszalekSejmu("marszaleksejmu@zp.pl", "marszalekmarszalek","Marszalek", "Sejmu", "00010102260", "HNE905783", "AAA111222", "ABC");
+        createSampleMarszalekSenatu("marszaleksenatu@zp.pl", "marszalekmarszalek","Marszalek", "Senatu", "02032857461", "BWY747731", "BBB111222", "DEF");
+        createSamplePosel("posel1@zp.pl", "posel1posel1", "Anna", "Nowak", "09122145291", "OHA621550", "AAA111111", "ABC");
+        createSamplePosel("posel2@zp.pl", "posel2posel2", "Jan", "Kowalski", "08030261521", "SOU105394", "AAA222111", "DEF");
+        createSamplePosel("posel3@zp.pl", "posel3posel3", "Bonifacy", "Wójcik", "09090474258", "RLD507081", "AAA333111", "FGH");
+        createSamplePosel("posel4@zp.pl", "posel4posel4", "Maurycy", "Wiśniewski", "08122579844", "TNV813623", "AAA444111", "FGH");
+        createSamplePosel("posel5@zp.pl", "posel5posel5", "Alfred", "Szymański", "01070493316", "MTE724079", "AAA555111", "ABC");
+        createSampleSenator("senator1@zp.pl", "senator1senator1", "Maria", "Woźniak", "00021086865", "MWQ431891", "BBB111111", "ABC");
+        createSampleSenator("senator2@zp.pl", "senator2senator2", "Luiza", "Zielińki", "01052466736", "FSB511566", "BBB222111", "DEF");
+        createSampleSenator("senator3@zp.pl", "senator3senator3", "Romuald", "Kowalczyk", "08100212961", "NWK819504", "BBB333111", "FGH");
+        createSampleSenator("senator4@zp.pl", "senator4senator4", "Alfons", "Szymczyk", "00111523793", "XFO160117", "BBB444111", "FGH");
+        createSampleSenator("senator5@zp.pl", "senator5senator5", "Rajmund", "Markowski", "07040132841", "NSH117627", "BBB555111", "ABC");
+
+
     }
 
     private void createSampleAdmin(String email, String password, String name, String surname, String pesel, String idnumber){
@@ -48,44 +65,52 @@ public class DBInitializer implements CommandLineRunner {
         cS.create(user);
     }
 
-    private void createSampleMarszalekSejmu(String email, String password, String name, String surname, String pesel, String idnumber){
+    private void createSampleMarszalekSejmu(String email, String password, String name, String surname, String pesel, String idnumber, String parlIdCard, String politicalGroup){
         CitizenEntity marszalek=  createBasicUser(email, password, name, surname,pesel, idnumber);
         String[] roles =  {"ROLE_USER", "ROLE_MARSZALEK_SEJMU", "ROLE_POSEL"};
         setRole(marszalek, roles);
         cS.create(marszalek);
         PoliticianEntity politician= new PoliticianEntity(marszalek);
-        pS.create(politician);
+        poS.create(politician);
+        ParliamentarianEntity parliamentarian= new ParliamentarianEntity(parlIdCard, politicalGroup, "Sejm", politician);
+        paS.create(parliamentarian);
     }
 
-    private void createSampleMarszalekSenatu(String email, String password, String name, String surname, String pesel, String idnumber){
+    private void createSampleMarszalekSenatu(String email, String password, String name, String surname, String pesel, String idnumber, String parlIdCard, String politicalGroup){
         CitizenEntity marszalek=  createBasicUser(email, password, name, surname,pesel, idnumber);
         String[] roles =  {"ROLE_USER", "ROLE_MARSZALEK_SENATU", "ROLE_SENATOR"};
         setRole(marszalek, roles);
         cS.create(marszalek);
         PoliticianEntity politician= new PoliticianEntity(marszalek);
-        pS.create(politician);
+        poS.create(politician);
+        ParliamentarianEntity parliamentarian= new ParliamentarianEntity(parlIdCard, politicalGroup, "Senat", politician);
+        paS.create(parliamentarian);
     }
 
-    private void createSampleSenator(String email, String password, String name, String surname, String pesel, String idnumber){
+    private void createSampleSenator(String email, String password, String name, String surname, String pesel, String idnumber, String parlIdCard, String politicalGroup){
         CitizenEntity senator=  createBasicUser(email, password, name, surname,pesel, idnumber);
         String[] roles =  {"ROLE_USER", "ROLE_SENATOR"};
         setRole(senator, roles);
         cS.create(senator);
-        PoliticianEntity s= new PoliticianEntity(senator);
-        pS.create(s);
+        PoliticianEntity politician= new PoliticianEntity(senator);
+        poS.create(politician);
+        ParliamentarianEntity parliamentarian= new ParliamentarianEntity(parlIdCard, politicalGroup, "Senat", politician);
+        paS.create(parliamentarian);
     }
 
-    private void createSamplePosel(String email, String password, String name, String surname, String pesel, String idnumber){
+    private void createSamplePosel(String email, String password, String name, String surname, String pesel, String idnumber, String parlIdCard, String politicalGroup){
         CitizenEntity politician=  createBasicUser(email, password, name, surname,pesel, idnumber);
         String[] roles =  {"ROLE_USER", "ROLE_POSEL"};
         setRole(politician, roles);
         cS.create(politician);
         PoliticianEntity p= new PoliticianEntity(politician);
-        pS.create(p);
+        poS.create(p);
+        ParliamentarianEntity parliamentarian= new ParliamentarianEntity(parlIdCard, politicalGroup, "Sejm", p);
+        paS.create(parliamentarian);
     }
 
     private void setRole(CitizenEntity citizen, String[] roles){
-        List<Role> r = new ArrayList<Role>();
+        List<Role> r = new ArrayList<>();
         for ( String role : roles ) {
             Optional<Role> temp = rR.findByName(role);
             temp.ifPresent(r::add);
