f021c2adcac408bb61800e41116c8a96952dd7ff
Maciej <m.nikiel@gmail.com>
2020-05-27 22:23:08 +0200

    Populate table with data
    Add logic in controller and thymeleaf atributes in html

--- src/main/java/edu/agh/zp/controller/UstawyController.java
+++ src/main/java/edu/agh/zp/controller/UstawyController.java
@@ -1,13 +1,22 @@
 package edu.agh.zp.controller;
 
+import edu.agh.zp.objects.DocumentEntity;
+import edu.agh.zp.repositories.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping (value={"/ustawy"})
 public class UstawyController {
+
+	@Autowired
+	DocumentRepository documentRepository;
 
 	@GetMapping (value = {""})
 	public ModelAndView index() {
@@ -16,9 +25,11 @@ public class UstawyController {
 		return modelAndView;
 	}
 
-	@GetMapping (value = {"dziennikUstaw"})
+	@GetMapping (value = {"/dziennikUstaw"})
 	public ModelAndView documentList() {
 		ModelAndView modelAndView = new ModelAndView( );
+		List< DocumentEntity > documents = documentRepository.findAll();
+		modelAndView.addObject( "documents", documents );
 		modelAndView.setViewName( "documentList" );
 		return modelAndView;
 	}
