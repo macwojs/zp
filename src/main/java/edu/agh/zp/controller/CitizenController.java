bea47bdaa3e71547443b417e02ddcbe5c5fa76f4
Maciej <m.nikiel@gmail.com>
2020-05-27 17:58:50 +0200

    Populate page in controller

--- src/main/java/edu/agh/zp/controller/CitizenController.java
+++ src/main/java/edu/agh/zp/controller/CitizenController.java
@@ -1,17 +1,32 @@
 package edu.agh.zp.controller;
+
+import edu.agh.zp.repositories.CitizenRepository;
+import edu.agh.zp.objects.CitizenEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
+import java.util.Optional;
+
 @Controller
-@RequestMapping( value = { "/obywatel" } )
-public class CitizenController  {
+@RequestMapping ( value = { "/obywatel" } )
+public class CitizenController {
 
-	@GetMapping(value = {"dane"})
-	public ModelAndView citizenDetails(){
-		ModelAndView model = new ModelAndView();
+	@Autowired
+	private CitizenRepository citizenRepository;
 
+	@GetMapping ( value = { "dane" } )
+	public ModelAndView citizenDetails( Principal principal ) {
+		ModelAndView model = new ModelAndView( );
+		Optional< CitizenEntity > currentCitizen = citizenRepository.findByEmail( principal.getName( ) );
+	    if (currentCitizen.isPresent())
+	    {
+	    	model.addObject( "citizen", currentCitizen.get() );
+	    }
+	    model.setViewName( "citizenDetails" );
 		return model;
 	}
 
