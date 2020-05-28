951e95786bac168e782bc35d52eeb5fad4ec7f8e
GrzegorzDomaradzki <grzedom@onet.pl>
2020-05-28 20:23:46 +0200

    Database changed - statuses names changed to match real laws.

--- src/main/java/edu/agh/zp/repositories/DocumentRepository.java
+++ src/main/java/edu/agh/zp/repositories/DocumentRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<DocumentEntity, Long> {
-	@Query (value="Select * from document WHERE (doctypeid=1 or doctypeid=4 or doctypeid=5 or doctypeid=6 or doctypeid=7) and (docstatusid=1 or docstatusid=5 or docstatusid=6 or docstatusid=7 or docstatusid=9)", nativeQuery=true)
+	@Query (value="Select * from document WHERE (doctypeid=1 or doctypeid=4 or doctypeid=5 or doctypeid=6 or doctypeid=7) and (docstatusid=1 or docstatusid=5 or docstatusid=6 or docstatusid=8 or docstatusid=9)", nativeQuery=true)
 	List<DocumentEntity> findByDocForSejm( );
 
 	@Query (value="Select * from document WHERE (doctypeid=1 or doctypeid=4 or doctypeid=5 or doctypeid=6 or doctypeid=7) and (docstatusid=2)", nativeQuery=true)
