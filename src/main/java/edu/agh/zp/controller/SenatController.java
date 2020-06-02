12b23d4c3a1a42f120f15f0f73bca3d1a5982963
Maciej <m.nikiel@gmail.com>
2020-06-02 21:49:52 +0200

    Add voting schedule to SenatController

--- src/main/java/edu/agh/zp/controller/SenatController.java
+++ src/main/java/edu/agh/zp/controller/SenatController.java
@@ -10,22 +10,18 @@ import edu.agh.zp.repositories.VotingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.sql.Date;
 import java.sql.Time;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Controller
 @RequestMapping ( value = { "/parlament/senat" } )
@@ -43,18 +39,18 @@ public class SenatController {
 	@GetMapping ( value = { "" } )
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView( );
-		createVotingList.run( modelAndView, Collections.singletonList(VotingEntity.TypeOfVoting.SENAT), votingRepository );
+		createVotingList.run( modelAndView, Collections.singletonList( VotingEntity.TypeOfVoting.SENAT ), votingRepository );
 		modelAndView.setViewName( "senat" );
 		return modelAndView;
 	}
 
 	@GetMapping ( value = { "/voteAdd" } )
 	public ModelAndView sejmVoteAdd( ModelAndView model ) {
-		List< DocumentEntity > documents = documentRepository.findByDocForSenat();
-		Optional< SetEntity > set = setRepository.findById((long)1);
-		if(set.isPresent()) {
-			model.addObject("documents", documents);
-			model.addObject("voting", new VotingEntity());
+		List< DocumentEntity > documents = documentRepository.findByDocForSenat( );
+		Optional< SetEntity > set = setRepository.findById( (long) 1 );
+		if ( set.isPresent( ) ) {
+			model.addObject( "documents", documents );
+			model.addObject( "voting", new VotingEntity( ) );
 		}
 		model.setViewName( "senatVotingAdd" );
 		return model;
@@ -63,32 +59,53 @@ public class SenatController {
 	@PostMapping ( value = { "/voteAdd" } )
 	public ModelAndView documentFormSubmit( @Valid @ModelAttribute ( "voting" ) VotingEntity voting, BindingResult res ) throws ParseException {
 		if ( res.hasErrors( ) ) {
-			for( Object i : res.getAllErrors()){
-				System.out.print("\n"+i.toString()+"\n");
+			for ( Object i : res.getAllErrors( ) ) {
+				System.out.print( "\n" + i.toString( ) + "\n" );
 			}
 
 			//Musi być ponownie dodane, bo inaczej nie wypełnia listy
-			ModelAndView model = new ModelAndView(  );
-			List< DocumentEntity > documents = documentRepository.findByDocForSenat();
-			model.addObject("documents", documents);
+			ModelAndView model = new ModelAndView( );
+			List< DocumentEntity > documents = documentRepository.findByDocForSenat( );
+			model.addObject( "documents", documents );
 			model.setViewName( "senatVotingAdd" );
 			return model;
 		}
 		Optional< SetEntity > set = setRepository.findById( (long) 2 );
-		if(set.isPresent()) {
-			voting.setSetID_column(set.get());
-			DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+		if ( set.isPresent( ) ) {
+			voting.setSetID_column( set.get( ) );
+			DateFormat formatter = new SimpleDateFormat( "HH:mm:ss" );
 
-			Time timeValueOpen = new Time(formatter.parse(voting.getOpen()).getTime());
-			Time timeValueClose = new Time(formatter.parse(voting.getClose()).getTime());
-			voting.setCloseVoting(timeValueClose);
-			voting.setOpenVoting(timeValueOpen);
+			Time timeValueOpen = new Time( formatter.parse( voting.getOpen( ) ).getTime( ) );
+			Time timeValueClose = new Time( formatter.parse( voting.getClose( ) ).getTime( ) );
+			voting.setCloseVoting( timeValueClose );
+			voting.setOpenVoting( timeValueOpen );
 
-			voting.setVotingType(VotingEntity.TypeOfVoting.SENAT);
-			votingRepository.save(voting);
+			voting.setVotingType( VotingEntity.TypeOfVoting.SENAT );
+			votingRepository.save( voting );
 		}
 		RedirectView redirect = new RedirectView( );
 		redirect.setUrl( "/parlament/senat" );
 		return new ModelAndView( redirect );
+	}
+
+	@GetMapping ( value = { "/votingSchedule" } )
+	public ModelAndView votingSchedule( HttpServletRequest request, @RequestParam ( value = "dateForm", required = false ) Date dateForm ) {
+		ModelAndView model = new ModelAndView( );
+		java.util.Date date = new java.util.Date( );
+		if ( dateForm != null ) {
+			date = dateForm;
+		}
+		Date dateSQL = new Date( date.getTime( ) );
+		List< Long > statusID = Collections.singletonList( 2L );
+		List< VotingEntity > votings = votingRepository.findByVotingDateAndDocumentIDDocStatusIDDocStatusIDIn( dateSQL, statusID );
+		String pattern = "dd MMMMM yyyy";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat( pattern, new Locale( "pl", "PL" ) );
+		System.out.println( date );
+		String formattedDate = simpleDateFormat.format( date );
+		model.addObject( "schedule_name", "Senacie" );
+		model.addObject( "current_date", formattedDate );
+		model.addObject( "votings", votings );
+		model.setViewName( "votingSchedule" );
+		return model;
 	}
 }
