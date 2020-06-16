8cab0623c343ac08d399a9ac883a218124576afa
pablito_chmiel <pawcio27610@wp.pl>
2020-06-16 02:12:56 +0200

    add view to fn person

--- src/main/java/edu/agh/zp/repositories/RoleRepository.java
+++ src/main/java/edu/agh/zp/repositories/RoleRepository.java
@@ -1,13 +1,17 @@
 package edu.agh.zp.repositories;
 
+import edu.agh.zp.objects.CitizenEntity;
 import edu.agh.zp.objects.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RoleRepository  extends JpaRepository<Role, Long> {
     Optional<Role> findById(long roleid);
     Optional<Role> findByName(String name);
+    List<Role> findByUsersIsIn(List<CitizenEntity> users);
 }
